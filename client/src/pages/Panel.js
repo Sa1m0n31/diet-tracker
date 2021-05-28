d888a22675274ae8e0c863159466da5eb3aad29d
Szymon Burak <sajmon0031@gmail.com>
2021-05-28 02:53:02 +0200

    Switch from React Charts to Chart.js library

--- client/src/pages/Panel.js
+++ client/src/pages/Panel.js
@@ -3,11 +3,13 @@ import TopMenu from "../components/TopMenu";
 import axios from 'axios'
 import useDemoConfig from '../helpers/useDemoConfig';
 import { Chart } from 'react-charts';
+import { Line } from 'react-chartjs-2'
 
 import auth from "../helpers/auth";
 
 const Panel = () => {
     let [loggedIn, setLoggedIn] = useState(false);
+    let [currentDay, setCurrentDay] = useState(new Date());
     let [calories, setCalories] = useState(parseInt(localStorage.getItem('diet-tracker-cpm')));
     let [protein, setProtein] = useState(parseInt(localStorage.getItem('diet-tracker-protein')));
     let [carbo, setCarbo] = useState(parseInt(localStorage.getItem('diet-tracker-carbo')));
@@ -76,145 +78,128 @@ const Panel = () => {
         console.log(data);
     }, [data]);
 
-    const data_calories = React.useMemo(
-        () => [
+    const last7Days = () => {
+        const currentDate = new Date();
+        let today = new Date();
+        currentDate.setDate(today.getDate()-7);
+        const minus1 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1);
+        currentDate.setDate(today.getDate()-6);
+        currentDate.setMonth(today.getMonth());
+        const minus2 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+        currentDate.setDate(today.getDate()-5);
+        currentDate.setMonth(today.getMonth());
+        const minus3 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+        currentDate.setDate(today.getDate()-4);
+        currentDate.setMonth(today.getMonth());
+        const minus4 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+        currentDate.setDate(today.getDate()-3);
+        currentDate.setMonth(today.getMonth());
+        const minus5 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+        currentDate.setDate(today.getDate()-2);
+        currentDate.setMonth(today.getMonth());
+        const minus6 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+        currentDate.setDate(today.getDate()-1);
+        currentDate.setMonth(today.getMonth());
+        const minus7 = currentDate.getDate() + "." + parseInt(currentDate.getMonth()+1)
+
+        return [minus1, minus2, minus3, minus4, minus5, minus6, minus7]
+    }
+
+    const dataCalories = {
+        labels: last7Days(),
+        datasets: [
             {
-                label: 'Wynik',
-                data: [
-                    { primary: 1, secondary: parseInt(data[6].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 2, secondary: parseInt(data[5].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 3, secondary: parseInt(data[4].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 4, secondary: parseInt(data[3].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 5, secondary: parseInt(data[2].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 6, secondary: parseInt(data[1].kilokalorie * data[0].ilosc / 100) },
-                    { primary: 7, secondary: parseInt(data[0].kilokalorie * data[0].ilosc / 100) },
-                ],
+                label: "Zalecane dzienne spozycie",
+                data: [calories, calories, calories, calories, calories, calories, calories],
+                backgroundColor: 'rgb(255, 99, 132)'
             },
             {
-                label: 'Zalecane dzienne spozycie',
+                label: "Wynik",
                 data: [
-                    { primary: 1, secondary: calories },
-                    { primary: 2, secondary: calories },
-                    { primary: 3, secondary: calories },
-                    { primary: 4, secondary: calories },
-                    { primary: 5, secondary: calories },
-                    { primary: 6, secondary: calories },
-                    { primary: 7, secondary: calories }
+                    data[6].kilokalorie * data[0].ilosc / 100,
+                    data[5].kilokalorie * data[0].ilosc / 100,
+                    data[4].kilokalorie * data[0].ilosc / 100,
+                    data[3].kilokalorie * data[0].ilosc / 100,
+                    data[2].kilokalorie * data[0].ilosc / 100,
+                    data[1].kilokalorie * data[0].ilosc / 100,
+                    data[0].kilokalorie * data[0].ilosc / 100
                 ],
+                backgroundColor: '#010101'
             }
-        ],
-        [data]
-    )
+        ]
+    }
 
-    const data_protein = React.useMemo(
-        () => [
+    const dataProtein = {
+        labels: last7Days(),
+        datasets: [
             {
-                label: 'Wynik',
-                data: [
-                    { primary: 1, secondary: parseInt(data[6].bialka * data[0].ilosc / 100) },
-                    { primary: 2, secondary: parseInt(data[5].bialka * data[0].ilosc / 100) },
-                    { primary: 3, secondary: parseInt(data[4].bialka * data[0].ilosc / 100) },
-                    { primary: 4, secondary: parseInt(data[3].bialka * data[0].ilosc / 100) },
-                    { primary: 5, secondary: parseInt(data[2].bialka * data[0].ilosc / 100) },
-                    { primary: 6, secondary: parseInt(data[1].bialka * data[0].ilosc / 100) },
-                    { primary: 7, secondary: parseInt(data[0].bialka * data[0].ilosc / 100) }
-                ],
+                label: "Zalecane dzienne spozycie",
+                data: [protein, protein, protein, protein, protein, protein, protein],
+                backgroundColor: 'rgb(255, 99, 132)'
             },
             {
-                label: 'Zalecane dzienne spozycie',
+                label: "Wynik",
                 data: [
-                    { primary: 1, secondary: protein },
-                    { primary: 2, secondary: protein },
-                    { primary: 3, secondary: protein },
-                    { primary: 4, secondary: protein },
-                    { primary: 5, secondary: protein },
-                    { primary: 6, secondary: protein },
-                    { primary: 7, secondary: protein }
+                    data[6].bialka * data[0].ilosc / 100,
+                    data[5].bialka * data[0].ilosc / 100,
+                    data[4].bialka * data[0].ilosc / 100,
+                    data[3].bialka * data[0].ilosc / 100,
+                    data[2].bialka * data[0].ilosc / 100,
+                    data[1].bialka * data[0].ilosc / 100,
+                    data[0].bialka * data[0].ilosc / 100
                 ],
+                backgroundColor: '#010101'
             }
-        ],
-        [data]
-    )
+        ]
+    }
 
-    const data_carbo = React.useMemo(
-        () => [
+    const dataCarbo = {
+        labels: last7Days(),
+        datasets: [
             {
-                label: 'Wynik',
-                data: [
-                    { primary: 1, secondary: parseInt(data[6].weglowodany * data[0].ilosc / 100) },
-                    { primary: 2, secondary: parseInt(data[5].weglowodany * data[0].ilosc / 100) },
-                    { primary: 3, secondary: parseInt(data[4].weglowodany * data[0].ilosc / 100) },
-                    { primary: 4, secondary: parseInt(data[3].weglowodany * data[0].ilosc / 100) },
-                    { primary: 5, secondary: parseInt(data[2].weglowodany * data[0].ilosc / 100) },
-                    { primary: 6, secondary: parseInt(data[1].weglowodany * data[0].ilosc / 100) },
-                    { primary: 7, secondary: parseInt(data[0].weglowodany * data[0].ilosc / 100) },
-                ],
+                label: "Zalecane dzienne spozycie",
+                data: [carbo, carbo, carbo, carbo, carbo, carbo, carbo],
+                backgroundColor: 'rgb(255, 99, 132)'
             },
             {
-                label: 'Zalecane dzienne spozycie',
+                label: "Wynik",
                 data: [
-                    { primary: 1, secondary: carbo },
-                    { primary: 2, secondary: carbo },
-                    { primary: 3, secondary: carbo },
-                    { primary: 4, secondary: carbo },
-                    { primary: 5, secondary: carbo },
-                    { primary: 6, secondary: carbo },
-                    { primary: 7, secondary: carbo }
+                    data[6].weglowodany * data[0].ilosc / 100,
+                    data[5].weglowodany * data[0].ilosc / 100,
+                    data[4].weglowodany * data[0].ilosc / 100,
+                    data[3].weglowodany * data[0].ilosc / 100,
+                    data[2].weglowodany * data[0].ilosc / 100,
+                    data[1].weglowodany * data[0].ilosc / 100,
+                    data[0].weglowodany * data[0].ilosc / 100
                 ],
+                backgroundColor: '#010101'
             }
-        ],
-        [data]
-    )
+        ]
+    }
 
-    const data_fiber = React.useMemo(
-        () => [
+    const dataFiber = {
+        labels: last7Days(),
+        datasets: [
             {
-                label: 'Wynik',
-                data: [
-                    { primary: 1, secondary: parseInt(data[6].blonnik * data[0].ilosc / 100) },
-                    { primary: 2, secondary: parseInt(data[5].blonnik * data[0].ilosc / 100) },
-                    { primary: 3, secondary: parseInt(data[4].blonnik * data[0].ilosc / 100) },
-                    { primary: 4, secondary: parseInt(data[3].blonnik * data[0].ilosc / 100) },
-                    { primary: 5, secondary: parseInt(data[2].blonnik * data[0].ilosc / 100) },
-                    { primary: 6, secondary: parseInt(data[1].blonnik * data[0].ilosc / 100) },
-                    { primary: 7, secondary: parseInt(data[0].blonnik * data[0].ilosc / 100) },
-                ],
+                label: "Zalecane dzienne spozycie",
+                data: [30, 30, 30, 30, 30, 30, 30],
+                backgroundColor: 'rgb(255, 99, 132)'
             },
             {
-                label: 'Zalecane dzienne spozycie',
+                label: "Wynik",
                 data: [
-                    { primary: 1, secondary: 30 },
-                    { primary: 2, secondary: 30 },
-                    { primary: 3, secondary: 30 },
-                    { primary: 4, secondary: 30 },
-                    { primary: 5, secondary: 30 },
-                    { primary: 6, secondary: 30 },
-                    { primary: 7, secondary: 30 }
+                    data[6].blonnik * data[0].ilosc / 100,
+                    data[5].blonnik * data[0].ilosc / 100,
+                    data[4].blonnik * data[0].ilosc / 100,
+                    data[3].blonnik * data[0].ilosc / 100,
+                    data[2].blonnik * data[0].ilosc / 100,
+                    data[1].blonnik * data[0].ilosc / 100,
+                    data[0].blonnik * data[0].ilosc / 100
                 ],
+                backgroundColor: '#010101'
             }
-        ],
-        [data]
-    )
-
-    const series = React.useMemo(
-        () => ({
-            showPoints: false,
-        }),
-        []
-    );
-
-    const axes = React.useMemo(
-        () => [
-            {
-                primary: true,
-                type: "time",
-                position: "bottom",
-            },
-            { type: "linear", position: "left" },
-        ],
-        []
-    );
-
+        ]
+    }
 
     return <div className="container-fluid p-0">
         <TopMenu username={loggedIn ? localStorage.getItem('diet-tracker-login') : ""} />
@@ -229,28 +214,28 @@ const Panel = () => {
                     <h3 className="chartHeader">
                         Kilokalorie
                     </h3>
-                    <Chart data={data_calories} series={series} axes={axes} width={500} height={500} tooltip />
+                    <Line data={dataCalories} />
                 </div>
 
                 <div className="chartContainer">
                     <h3 className="chartHeader">
                         Białko
                     </h3>
-                    <Chart data={data_protein} series={series} axes={axes} width={500} height={500} tooltip />
+                    <Line data={dataProtein} />
                 </div>
 
                 <div className="chartContainer">
                     <h3 className="chartHeader">
                         Węglowodany
                     </h3>
-                    <Chart data={data_carbo} series={series} axes={axes} width={500} height={500} tooltip />
+                    <Line data={dataCarbo} />
                 </div>
 
                 <div className="chartContainer">
                     <h3 className="chartHeader">
                         Błonnik
                     </h3>
-                    <Chart data={data_fiber} series={series} axes={axes} width={500} height={500} tooltip />
+                    <Line data={dataFiber} />
                 </div>
             </div>
         </> : ""}
